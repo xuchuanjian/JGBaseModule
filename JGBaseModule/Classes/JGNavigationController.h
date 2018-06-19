98d1091974715118d84ec34193be4500e0e13f37
xuchuanjian <239330010@qq.com>
2018-06-19 17:51:40 +0800

    first commit

---
+++ JGBaseModule/Classes/JGNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  JGNavigationController.h
+//  JGBaseModule_Example
+//
+//  Created by 徐大大 on 2018/6/19.
+//  Copyright © 2018年 xuchuanjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGNavigationController : UINavigationController
+
+@end
---
+++ JGBaseModule/Classes/JGTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  JGTabBarController.h
+//  JGBaseModule_Example
+//
+//  Created by 徐大大 on 2018/6/19.
+//  Copyright © 2018年 xuchuanjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGTabBarController : UITabBarController
+
+@end
